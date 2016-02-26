68189c9d96ac522f1b7bf8ed9fabaef41a0f2f47
Mauricio <maurovalistico@hotmail.com>
2016-02-26 08:48:03 -0500

    Cree las variables globales

--- app/src/main/java/talleruno_redes/tarea6/MainActivity.java
+++ app/src/main/java/talleruno_redes/tarea6/MainActivity.java
@@ -5,6 +5,7 @@ package talleruno_redes.tarea6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,11 +14,34 @@ public class MainActivity extends AppCompatActivity {
     EditText usuario, password;
     Button login, resgistro;
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void botonera(){
+
+        Button login = (Button) findViewById(R.id.logBut);
+        Button registro = (Button) findViewById(R.id.regBut);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
