046702a36efc0b8e5f177a436d7f0f7247a886cd
Mauricio <maurovalistico@hotmail.com>
2016-02-26 08:38:32 -0500

    Cree las variables globales

--- app/src/main/java/talleruno_redes/tarea6/MainActivity.java
+++ app/src/main/java/talleruno_redes/tarea6/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     EditText usuario, password;
     Button login, resgistro;
 
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
