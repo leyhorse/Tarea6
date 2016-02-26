05b842b39ef1d9334ecbabb7e7cc2c5e395b956b
Mauricio Valencia <Mauricio Valencia>
2016-02-26 08:26:45 -0500

    Initial commit

---
+++ app/src/main/java/talleruno_redes/tarea6/Comunicacion.java
@@ -0,0 +1,14 @@
+package talleruno_redes.tarea6;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Mauricio on 26/02/2016.
+ */
+public class Comunicacion implements Observer {
+    @Override
+    public void update(Observable observable, Object data) {
+
+    }
+}
